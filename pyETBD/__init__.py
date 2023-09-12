b4cbea5c350ab573b1588715bab82761275d5d89
Ryan Higginbotham <99490378+ryhigg@users.noreply.github.com>
2023-09-12 00:20:06 -0400

    fixed __init__.py

--- pyETBD/__init__.py
+++ pyETBD/__init__.py
@@ -1,4 +1,13 @@
 from pyETBD import organisms
 from pyETBD import schedules
 from pyETBD.algorithm import selection, recombination, mutation
-from pyETBD.utils import schedule_runners, equations, progress_bar, bin_converter, timer
+from pyETBD.utils import (
+    experiment_runner,
+    equations,
+    bin_converter,
+    experiment_builder,
+    experiment,
+    progress_bar,
+    schedule_runners,
+    timer,
+)
