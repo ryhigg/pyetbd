8cc5d57a5c2644dd8e88f9aeb65b90e3bce81eac
Ryan Higginbotham <99490378+ryhigg@users.noreply.github.com>
2023-09-12 00:15:35 -0400

    Updated comments

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from pyETBD.utils.experiment_runner import ExperimentRunner
 def main():
     # define the input file and output directory
     input_file = "example_experiment_inputs/mcdowell_et_al_2008_phase1.json"
+
+    # putting an empty string will output the same directory as the main.py file
+    # if you wanted to output to an 'outputs' directory in the same directory as main.py you would write:
+    # output_dir = "outputs/"
     output_dir = ""
 
     # create an instance of the ExperimentRunner class
