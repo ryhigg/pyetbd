762b5870aa3925bd2081f25aaed7c57cc3b3b09d
Ryan Higginbotham <99490378+ryhigg@users.noreply.github.com>
2023-09-12 11:03:16 -0400

    Updated inputs and comments in main.py

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from pyETBD.utils.experiment_runner import ExperimentRunner
 # define the main function
 def main():
     # define the input file and output directory
-    input_file = "example_experiment_inputs/mcdowell_et_al_2008_phase1.json"
+    input_file_path = "example_experiment_inputs/mcdowell_et_al_2008_phase1.json"
 
     # putting an empty string will output the same directory as the main.py file
-    # if you wanted to output to an 'outputs' directory in the same directory as main.py you would write:
-    # output_dir = "outputs/"
     output_dir = ""
+    # if you wanted to output to an 'outputs' directory in the same directory as main.py you would write:
+    # output_dir = "outputs/" (be sure to include the trailing slash)
+    # Note: the code will not create the directory for you, you must create it yourself
 
     # create an instance of the ExperimentRunner class
-    runner = ExperimentRunner(input_file, output_dir)
+    runner = ExperimentRunner(input_file_path, output_dir)
 
     # call the giddyup method to run the experiments
     runner.giddyup()
