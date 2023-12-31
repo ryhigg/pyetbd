49d6dbf3b1635a6521737218c51a98528757cc9c
Ryan Higginbotham <99490378+ryhigg@users.noreply.github.com>
2023-12-30 23:42:29 -0500

    added comments

--- gui_main.py
+++ gui_main.py
@@ -1,4 +1,8 @@
+# Description: This file is the main file for the GUI. It loads the GUI and runs it.
+
+# Loads the GUI for the experiment
 from pyETBD.experiment_gui import ExperimentGUI
 
+# create an instance of the ExperimentGUI class and call the run method
 gui = ExperimentGUI()
 gui.run()
