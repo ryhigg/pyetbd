236fe4143a3235c49a61c9e084c51b985951416d
Ryan Higginbotham <99490378+ryhigg@users.noreply.github.com>
2023-12-31 00:24:46 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,30 +2,18 @@
 
 
 from setuptools import setup, find_packages
-import codecs
-import os
 
 
-VERSION = "1.0.0"
-DESCRIPTION = "A package for implementing Mcdowell's (2004) ETBD"
-
-
-# Setting up
 setup(
     name="pyetbd",
-    version=VERSION,
-    author="Ryan Higginbotham (ryhigg)",
+    version="0.1.0",
+    author="Ryan Higginbotham",
     author_email="ryanhigginbotham@ufl.edu",
-    description=DESCRIPTION,
+    description="McDowell's (2004) ETBD implemented in Python",
     packages=find_packages(),
-    install_requires=["numpy", "pandas", "numba"],
-    keywords=["python", "ETBD"],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: People interested in the ETBD",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
+    install_requires=[
+        "numba==0.57.1",
+        "numpy==1.24.4",
+        "pandas==2.1.0",
     ],
 )
