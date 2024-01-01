c0ec3cb30e006d4868c03cf0407183ab8dae996e
Ryan Higginbotham <99490378+ryhigg@users.noreply.github.com>
2024-01-01 01:01:10 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@
 
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
+LONG_DESCRIPTION = "This is an open-source Python package to implement McDowell's (2004) Evolutionary Theory of Behavior Dynamics. The package is currently under development. This version has successfully replicated the results of McDowell et al. (2008). It is not recommended to run experiments for publication until the package has been validated by replicating more results from the literature."
 
 setup(
     name="pyetbd",
@@ -13,8 +11,7 @@ setup(
     author="Ryan Higginbotham",
     author_email="ryanhigginbotham@ufl.edu",
     description="McDowell's (2004) ETBD implemented in Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=[
         "numba==0.57.1",
