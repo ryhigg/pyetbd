26dcabb116fbe13ffe33534282d1a0d386b204c2
ryhigg <rehigginbotham3711@gmail.com>
2024-01-26 14:28:12 -0500

    Remove GUI and example script files

--- gui_main.py
+++
@@ -1,8 +0,0 @@
-# Description: This file is the main file for the GUI. It loads the GUI and runs it.
-
-# Loads the GUI for the experiment
-from pyetbd.experiment_gui import ExperimentGUI
-
-# create an instance of the ExperimentGUI class and call the run method
-gui = ExperimentGUI()
-gui.run()
